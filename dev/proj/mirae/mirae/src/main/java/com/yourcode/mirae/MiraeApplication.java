13c6c37548ab7e44ebfce71ac459c65530112ce5
itsmeyji <jungyunji031023@gmail.com>
2025-12-19 18:03:27 +0900

    chore

---
+++ dev/proj/mirae/mirae/src/main/java/com/yourcode/mirae/MiraeApplication.java
@@ -0,0 +1,14 @@
+package com.yourcode.mirae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiraeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiraeApplication.class, args);
+
+	}
+
+}
